da911a7f26ce0fcbe5fb15c84b66cacf9a040b49
harishbb1 <35723205+harishbb1@users.noreply.github.com>
2018-10-10 12:07:02 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,11 @@
+public class Example{  
+   public static void main(String args[]){   
+	String str = "Javalab"; 
+	char arrch[]={'h','e','l','l','o'};
+	String str2 = new String(arrch); 
+	String str3 = new String("Javaprogrammer"); 
+	System.out.println(str);  
+	System.out.println(str2);  
+	System.out.println(str3);  
+   }
+}
